9bb05d6914352d529ade42c49bfb33c4d4918351
MutsaMyra <mutsa.myra07@gmail.com>
2026-07-02 14:37:06 +0200

    updated seeding user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,26 +5,42 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        User::create([
+        // Get roles (must exist from RolesAndPermissionsSeeder)
+        $adminRole = Role::where('name', 'Admin')->first();
+        $itRole = Role::where('name', 'IT')->first();
+        $managementRole = Role::where('name', 'Management')->first();
+
+        // ADMIN USER (FULL ACCESS)
+        $admin = User::create([
             'name' => 'Admin User',
             'email' => 'admin@hpc.com',
             'password' => Hash::make('password'),
-            'role' => 'admin',
         ]);
 
-        User::create([
-            'name' => 'Regular User',
-            'email' => 'user@hpc.com',
+        $admin->assignRole($adminRole);
+
+        // IT USER
+        $it = User::create([
+            'name' => 'IT User',
+            'email' => 'it@hpc.com',
             'password' => Hash::make('password'),
-            'role' => 'user',
         ]);
+
+        $it->assignRole($itRole);
+
+        // MANAGEMENT USER
+        $management = User::create([
+            'name' => 'Management User',
+            'email' => 'management@hpc.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $management->assignRole($managementRole);
     }
 }
